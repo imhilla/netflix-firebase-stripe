3db507988d006d87c88d4f3769767d50b2985dd8
imhilla <hillaryodhiambo282@gmail.com>
2021-02-08 19:45:19 +0300

    add banner img

--- src/Banner.js
+++ src/Banner.js
@@ -4,12 +4,16 @@ import "../src/Banner.css"
 function Banner() {
   return (
     <header
-    className="banner"
-     style={{
-      backgroundSize: "cover",
-      backgroundImage: `url('https://i.imgur.com/e1hLQ2m.png)`,
-      backgroundPosition: "center center"
-    }}>
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://i.imgur.com/e1hLQ2m.png)`,
+        backgroundPosition: "center center"
+      }}>
+
+      <div className="banner_contents">
+        <h1 className="banner_title">Movie name</h1>
+      </div>
     </header>
   )
 }
