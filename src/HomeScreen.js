02bf23a4d8a1efc47710496e93b328732afb74fb
imhilla <hillaryodhiambo282@gmail.com>
2021-02-08 16:29:26 +0300

    add logos

---
+++ src/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../src/Nav'
+import "../src/HomeScreen.css"
+
+function HomeScreen() {
+  return (
+    <div className="homeScreem">
+       <Nav />
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../src/Nav.css"
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img src="https://variety.com/wp-content/uploads/2020/05/netflix-logo.png?w=1024" alt="logo" />
+      <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="logo" />
+      <h1>This is the nav</h1>
+    </div>
+  )
+}
+
+export default Nav
