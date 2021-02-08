9eaa66785c24150cf67da32d4ff57f75a4dc6bef
imhilla <hillaryodhiambo282@gmail.com>
2021-02-08 15:34:47 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDrmg238RxfwYGeloyr8SCOpmO4nRXjhD8",
+  authDomain: "netflix-clone-d7de6.firebaseapp.com",
+  projectId: "netflix-clone-d7de6",
+  storageBucket: "netflix-clone-d7de6.appspot.com",
+  messagingSenderId: "719900017644",
+  appId: "1:719900017644:web:9fa6502f0ef94296e636c9"
+};
